64a79f75fa9abb0d3fb4dbe911d2ce4940720c2e
xsadia <lipearthur@hotmail.com>
2021-09-15 20:39:39 -0300

    add me query

--- src/graphql/schema/QueryType.ts
+++ src/graphql/schema/QueryType.ts
@@ -1,4 +1,6 @@
 import { GraphQLObjectType, GraphQLString } from "graphql";
+import { User } from "../../models/User";
+import { UserType } from "../user/UserType";
 
 export const QueryType = new GraphQLObjectType({
     name: 'Query',
@@ -8,6 +10,25 @@ export const QueryType = new GraphQLObjectType({
             type: GraphQLString,
             resolve: () => {
                 return 'Hello, world';
+            }
+        },
+        me: {
+            type: UserType,
+            resolve: async (root, args, { user }) => {
+
+                if (!user) {
+                    const me = null;
+                    return me;
+                }
+
+                const me = await User.findOne({ _id: user._id });
+
+                if (!me) {
+                    const me = null;
+                    return me;
+                }
+
+                return me;
             }
         }
     })
