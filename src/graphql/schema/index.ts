7317b28a43a248942ceb15d21a8be6ca7534af56
xsadia <lipearthur@hotmail.com>
2021-09-15 13:45:33 -0300

    add MutationType to schema

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -1,6 +1,8 @@
 import { GraphQLSchema } from "graphql";
+import { MutationType } from "./MutationType";
 import { QueryType } from "./QueryType";
 
 export const schema = new GraphQLSchema({
-    query: QueryType
+    query: QueryType,
+    mutation: MutationType
 });
