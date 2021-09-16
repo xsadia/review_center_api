e6c65fcb8154986d6692ffd808472eaa47b00bee
xsadia <lipearthur@hotmail.com>
2021-09-16 20:47:20 -0300

    add movieById and movieByName queries

--- src/graphql/schema/QueryType.ts
+++ src/graphql/schema/QueryType.ts
@@ -1,5 +1,7 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { Movie } from "../../models/Movie";
 import { User } from "../../models/User";
+import { MovieType } from "../movie/MovieType";
 import { UserType } from "../user/UserType";
 
 export const QueryType = new GraphQLObjectType({
@@ -14,6 +16,7 @@ export const QueryType = new GraphQLObjectType({
         },
         me: {
             type: UserType,
+            description: 'Recover logged user\'s informations',
             resolve: async (root, args, { user }) => {
 
                 if (!user) {
@@ -29,6 +32,51 @@ export const QueryType = new GraphQLObjectType({
                 }
 
                 return me;
+            }
+        },
+        movieById: {
+            type: MovieType,
+            description: 'Query for a movie by ID',
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve: async (root, { id }, ctx) => {
+                const movie = await Movie.findOne({ _id: id });
+
+                if (!movie) {
+                    const movie = null;
+                    return movie;
+                }
+
+                return movie;
+            }
+        },
+        movieByName: {
+            type: MovieType,
+            description: 'Query for a movie by name',
+            args: {
+                name: {
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve: async (root, { name }, ctx) => {
+                const movie = await Movie.findOne({
+                    $text: {
+                        $search: name,
+                        $language: "pt",
+                        $caseSensitive: false,
+                        $diacriticSensitive: false
+                    }
+                });
+
+                if (!movie) {
+                    const movie = null;
+                    return movie;
+                }
+
+                return movie;
             }
         }
     })
