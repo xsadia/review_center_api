f7566387a57f88f3550c6c66b25fca55e7dda8dd
xsadia <lipearthur@hotmail.com>
2021-09-16 01:56:29 -0300

    add Movie mutations to MutationType

--- src/graphql/schema/MutationType.ts
+++ src/graphql/schema/MutationType.ts
@@ -1,10 +1,12 @@
 import { GraphQLObjectType } from "graphql";
 import UserMutations from '../user/mutations';
+import MovieMutations from '../movie/mutations';
 
 export const MutationType = new GraphQLObjectType({
     name: 'Mutation',
     description: 'Root of all mutations',
     fields: () => ({
-        ...UserMutations
+        ...UserMutations,
+        ...MovieMutations
     })
 });
