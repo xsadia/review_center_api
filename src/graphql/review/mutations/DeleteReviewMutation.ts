b8a9de8774c2ff2d377cb0aa43abb3c24ca2d327
xsadia <lipearthur@hotmail.com>
2021-10-06 12:49:59 -0300

    :monkey:

--- src/graphql/review/mutations/DeleteReviewMutation.ts
+++ src/graphql/review/mutations/DeleteReviewMutation.ts
@@ -1,47 +1,49 @@
 import { GraphQLBoolean, GraphQLNonNull, GraphQLString } from "graphql";
-import { mutationWithClientMutationId } from "graphql-relay";
+import { fromGlobalId, mutationWithClientMutationId } from "graphql-relay";
 import { Review } from "../../../models/Review";
 
 export default mutationWithClientMutationId({
-    name: 'DeleteReview',
-    description: 'Delete review mutation',
-    inputFields: {
-        movieId: {
-            type: new GraphQLNonNull(GraphQLString)
-        }
+  name: "DeleteReview",
+  description: "Delete review mutation",
+  inputFields: {
+    movieId: {
+      type: new GraphQLNonNull(GraphQLString),
     },
-    mutateAndGetPayload: async ({ movieId }, { user }) => {
-        if (!user._id) {
-            return {
-                error: 'Permission denied',
-                success: false
-            };
-        }
-
-        const review = await Review.findOne({ movieId, userId: user._id });
-
-        if (!review) {
-            return {
-                error: 'Review not found',
-                success: false
-            };
-        }
-
-        await review.delete();
-
-        return {
-            error: null,
-            success: true
-        };
-    },
-    outputFields: {
-        error: {
-            type: GraphQLString,
-            resolve: ({ error }) => error
-        },
-        success: {
-            type: GraphQLBoolean,
-            resolve: ({ success }) => success
-        }
+  },
+  mutateAndGetPayload: async ({ movieId: globalId }, { user }) => {
+    if (!user._id) {
+      return {
+        error: "Permission denied",
+        success: false,
+      };
     }
+
+    const { id } = fromGlobalId(globalId);
+
+    const review = await Review.findOne({ movieId: id, userId: user._id });
+
+    if (!review) {
+      return {
+        error: "Review not found",
+        success: false,
+      };
+    }
+
+    await review.delete();
+
+    return {
+      error: null,
+      success: true,
+    };
+  },
+  outputFields: {
+    error: {
+      type: GraphQLString,
+      resolve: ({ error }) => error,
+    },
+    success: {
+      type: GraphQLBoolean,
+      resolve: ({ success }) => success,
+    },
+  },
 });
